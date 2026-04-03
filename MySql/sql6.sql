5cdf874329e9fb6d45f7685404a1c44c77c29529
Khemu Edage <khemuyadage@gmail.com>
2026-04-03 22:44:47 +0530

    sql practice

--- MySql/sql6.sql
+++ MySql/sql6.sql
@@ -53,3 +53,66 @@ GROUP BY department;
 SELECT department, MAX(salary) as min_sal FROM employees
 GROUP BY department;
 
+SELECT department, SUM(salary) AS total_sal FROM employees
+GROUP BY department;
+
+SELECT department,
+COUNT(*) AS total_emp,
+MIN(salary) AS min_sal,
+MAX(salary)AS max_sal,
+SUM(salary) AS total_sal,
+AVG(salary) AS avg
+FROM employees
+GROUP BY department;
+
+-- Having
+-- order of execution 
+-- from -> where->group by->having -> select -> orderby
+
+SELECT department,
+COUNT(*) AS total_emp,
+MIN(salary) AS min_sal,
+MAX(salary)AS max_sal,
+SUM(salary) AS total_sal,
+AVG(salary) AS avg
+FROM employees
+GROUP BY department
+HAVING total_sal > 260000;
+
+
+-- department with less than 5 employees
+
+SELECT department,
+COUNT(*) AS total_emp
+FROM employees
+GROUP BY department
+HAVING total_emp < 6;
+
+SELECT department,
+COUNT(*) AS total_emp
+FROM employees
+GROUP BY department
+HAVING total_emp >= 2;
+
+
+
+SELECT * FROM cart;
+
+CREATE TABLE transactions
+(id INT PRIMARY KEY auto_increment,
+ product VARCHAR(30), 
+ region VARCHAR(30),
+ qauntity INT);
+ 
+ 
+ INSERT INTO transactions (product, region, qauntity)
+ VALUES
+ ("Laptop", "North", 5),
+ ("Laptop", "south", 7),
+ ("phone", "North", 10),
+ ("phone", "south", 8),
+ ("Laptop", "North", 6);
+
+
+
+
